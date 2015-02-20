9a27c9741c383b3bd71e9d9367a118b5bf2c7e6a
flask.alobet@gmail.com <flask.alobet@gmail.com>
2015-02-20 22:42:20 +0700

    flask use flask-migrate extension

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 import os
 from app import create_app
+from app import db
+from app.models import User, Role
 from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'development')
 manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()

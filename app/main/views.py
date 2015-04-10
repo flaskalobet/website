183d0fd4caec37f2671a0de5f5230d22753ad76d
flaskalobet <flask.alobet@gmail.com>
2015-04-10 15:04:43 +0700

    reset default

--- app/main/views.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from flask import render_template, session, redirect, url_for
-from . import main
-
-@main.route('/', methods=['GET', 'POST'])
-def index():
-    #return "<h1>Hello World!!!</h1>"
-    return render_template('index.html')

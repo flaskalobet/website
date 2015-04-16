3cdc51d0ecbd3587e81500d6411bd69474e25d4b
flaskalobet <flask.alobet@gmail.com>
2015-04-16 16:00:31 +0700

    flask script

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
+from flask.ext.script import Manager
+
 from config import config
 
 from app import create_app, db
 
 app = create_app('default')
-
+manager = Manager(app)
 
 if __name__ == '__main__':
-    app.run(host=config['server'])
+    #app.run(host=config['server'])
+    manager.run()
